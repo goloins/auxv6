7192904a26cfebfb59bab59232e27a60fd6f24bd
goloins <dsides.personal@gmail.com>
2026-03-26 15:52:39 -0500

    printf improvements

--- user/printf.c
+++ user/printf.c
@@ -3,13 +3,26 @@
 #include "../include/user.h"
 
 static void
-putc(int fd, char c)
+bufflush(int fd, char *out, int *pos)
 {
-  write(fd, &c, 1);
+  if(*pos > 0){
+    write(fd, out, *pos);
+    *pos = 0;
+  }
 }
 
 static void
-printint(int fd, int xx, int base, int sgn)
+bufputc(int fd, char *out, int *pos, int max, char c)
+{
+  if(*pos >= max)
+    bufflush(fd, out, pos);
+  out[(*pos)++] = c;
+  if(c == '\n')
+    bufflush(fd, out, pos);
+}
+
+static void
+printint(int fd, char *out, int *pos, int max, int xx, int base, int sgn)
 {
   static char digits[] = "0123456789ABCDEF";
   char buf[16];
@@ -32,18 +45,21 @@ printint(int fd, int xx, int base, int sgn)
     buf[i++] = '-';
 
   while(--i >= 0)
-    putc(fd, buf[i]);
+    bufputc(fd, out, pos, max, buf[i]);
 }
 
 // Print to the given fd. Only understands %d, %x, %p, %s.
 void
 printf(int fd, const char *fmt, ...)
 {
+  char out[256];
   char *s;
   int c, i, state;
+  int pos;
   uint *ap;
 
   state = 0;
+  pos = 0;
   ap = (uint*)(void*)&fmt + 1;
   for(i = 0; fmt[i]; i++){
     c = fmt[i] & 0xff;
@@ -51,14 +67,14 @@ printf(int fd, const char *fmt, ...)
       if(c == '%'){
         state = '%';
       } else {
-        putc(fd, c);
+        bufputc(fd, out, &pos, sizeof(out), c);
       }
     } else if(state == '%'){
       if(c == 'd'){
-        printint(fd, *ap, 10, 1);
+        printint(fd, out, &pos, sizeof(out), *ap, 10, 1);
         ap++;
       } else if(c == 'x' || c == 'p'){
-        printint(fd, *ap, 16, 0);
+        printint(fd, out, &pos, sizeof(out), *ap, 16, 0);
         ap++;
       } else if(c == 's'){
         s = (char*)*ap;
@@ -66,20 +82,22 @@ printf(int fd, const char *fmt, ...)
         if(s == 0)
           s = "(null)";
         while(*s != 0){
-          putc(fd, *s);
+          bufputc(fd, out, &pos, sizeof(out), *s);
           s++;
         }
       } else if(c == 'c'){
-        putc(fd, *ap);
+        bufputc(fd, out, &pos, sizeof(out), *ap);
         ap++;
       } else if(c == '%'){
-        putc(fd, c);
+        bufputc(fd, out, &pos, sizeof(out), c);
       } else {
         // Unknown % sequence.  Print it to draw attention.
-        putc(fd, '%');
-        putc(fd, c);
+        bufputc(fd, out, &pos, sizeof(out), '%');
+        bufputc(fd, out, &pos, sizeof(out), c);
       }
       state = 0;
     }
   }
+
+  bufflush(fd, out, &pos);
 }
